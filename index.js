13a5b51c7c7a81961d1c9f49f45e7876d4edce76
Akash8433 <akashroy38281@gmail.com>
2023-10-16 20:37:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 7000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//export syle and scripts from sub pages into the layout
+app.set()
+
+// use express router
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
